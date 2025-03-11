b944e897bac3957b18bd4d42450715044e707a6e
sinovcicmatija <163353550+sinovcicmatija@users.noreply.github.com>
2025-03-11 12:37:49 +0100

    Remove get WindDirection function from WeatherService

--- backend/WeatherAPI/WeatherAPI/Services/WeatherService.cs
+++ backend/WeatherAPI/WeatherAPI/Services/WeatherService.cs
@@ -26,17 +26,5 @@ namespace WeatherAPI.Services
             var weatherData = JsonSerializer.Deserialize<WeatherData>(response);
             return weatherData;
         }
-        public string GetWindDirection(int deg)
-        {
-            if (deg >= 337.5 || deg <= 22.5) return "N";
-            if (deg >= 22.5 && deg < 67.5) return "NE"; 
-            if (deg >= 67.5 && deg < 112.5) return "E";   
-            if (deg >= 112.5 && deg < 157.5) return "SE"; 
-            if (deg >= 157.5 && deg < 202.5) return "S";   
-            if (deg >= 202.5 && deg < 247.5) return "SW"; 
-            if (deg >= 247.5 && deg < 292.5) return "W"; 
-            if (deg >= 292.5 && deg < 337.5) return "NW"; 
-            return "Unknown";
-        }
     }
 }
